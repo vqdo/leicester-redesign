9ba8f0bfb1d01ad38c1c53f8f98dcec640fc59ef
tahzhang <tanyazhangcg@yahoo.com>
2014-11-25 04:13:17 +0000

    Safety save

--- js/master.js
+++ js/master.js
@@ -5,6 +5,7 @@ var cogs = (function() {
     self.init = function() {
         console.log("Document ready.");
         self.attachNavHandlers();
+        //self.doSlideshow();
     }
     
     self.attachNavHandlers = function() {
@@ -45,6 +46,51 @@ var cogs = (function() {
             
         });
     } 
+    
+    /* broken */
+    self.doSlideshow = function() {
+        console.log("Doing slideshow");
+        var parseSlideName = function(el) {
+            var classes = $(el).attr('class').split(/\s+/);
+            return classes[1]; // :(        
+        }
+        
+        // hardcode the slides in for now
+        var slides = ['study-with-us', 'research-with-us', 'business-and-enterprise', 'alumni'];
+        var bg = $('.slideshow-bg');
+        var panels = $('.slideshow-panel');
+        var leftButton = $('.slideshow-nav .left');
+        var rightButton = $('.slideshow-nav .right');
+        
+        var slidePos = 0;
+        var numSlides = slides.length;
+        
+        leftButton.click(function() {
+          var prev = slides[slidePos];
+          slidePos = (slidePos + numSlides - 1) % numSlides; 
+          var next = slides[slidePos];
+          var panel = $('.slideshow-panel.' + next);
+          
+          panels.hide();
+          panel.fadeIn();   
+ 
+          bg.switchClass(prev, next, 400);   
+        });
+        rightButton.click(function() {
+          var prev = slides[slidePos];
+          slidePos = (slidePos + 1) % numSlides; 
+          var next = slides[slidePos];
+          //var name = parseSlideName(next);
+          var panel = $('.slideshow-panel.' + next);
+          
+          bg.switchClass(prev, next);
+          
+          panels.hide();
+          panel.fadeIn();
+          
+        });
+        
+    }
     
     return self;
 })()
